97bac76b1814565e6fc3caa7f9aec1ca1b0f1e67
tonistorres <sisvenda2011@gmail.com>
2022-01-10 15:54:27 -0300

    feat:Implementando css Tela Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,20 @@ import '../App.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-container-main">
+        <div className="login-container-conteudo">
+          <label htmlFor="campo-email">
+            <p>Email:</p>
+            <input type="email" id="campo-email" name="email" />
+          </label>
+          <label htmlFor="campo-password">
+            <p>Senha:</p>
+            <input type="password" id="campo-password" name="password" />
+          </label>
+        </div>
+      </div>
+    );
   }
 }
 
