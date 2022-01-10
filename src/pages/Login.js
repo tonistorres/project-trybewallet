7f15c1f7c501ec3f77a0d02d0c2f4cb619c2a937
tonistorres <sisvenda2011@gmail.com>
2022-01-10 16:52:22 -0300

    feat:Propriedade disabled implementada no button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,21 +3,49 @@ import '../App.css';
 import perfil from '../assets/perfilLinkedin.jpeg';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabled: true,
+    };
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div className="login-container-main">
         <div className="login-container-conteudo">
           <div className="login-container-tech">
             <img src={ perfil } alt="Perfil Dev" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="js" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" />
-            <img src="https://img.icons8.com/color/48/000000/redux.png" alt="redux" />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+              alt="html"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+              alt="css"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+              alt="js"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+              alt="react"
+            />
+            <img
+              src="https://img.icons8.com/color/48/000000/redux.png"
+              alt="redux"
+            />
           </div>
           <label htmlFor="campo-email">
             <p>Email:</p>
-            <input type="email" data-testid="email-input" id="campo-email" name="email" />
+            <input
+              type="email"
+              data-testid="email-input"
+              id="campo-email"
+              name="email"
+            />
           </label>
           <label htmlFor="campo-password">
             <p>Senha:</p>
@@ -28,7 +56,9 @@ class Login extends React.Component {
               name="password"
             />
           </label>
-          <button type="button"> Entrar </button>
+          <button type="button" disabled={ disabled }>
+            Entrar
+          </button>
         </div>
       </div>
     );
